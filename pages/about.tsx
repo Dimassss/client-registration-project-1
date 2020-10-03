f28780b95b1596221614b2d1a718560b83d17908
Dimas <dimas.karpusss@gmail.com>
2020-10-03 23:30:14 +0300

    =/about page
    Done /about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,64 @@
+import { Button, createStyles, Grid, makeStyles, Paper } from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles(theme => createStyles({
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        marginBottom: theme.spacing(3)
+    },
+    link: {
+        padding: theme.spacing(1)
+    },
+    linkContainer: {
+        padding: theme.spacing(5)
+    }
+}))
+
 export default function BasicTextFields() {
+    const classes = useStyles();
+    const data = {
+        links: [
+            { href: 'https://t.me/grejtal', title: 'Telegram' },
+            { href: 'https://github.com/Dimassss', title: 'Github' },
+            { href: 'https://www.linkedin.com/in/pilevoid/', title: 'LinkedIn' },
+            { href: '/static/Dmytro_Karpus_CV.pdf', title: 'My CV' }
+        ]
+    }
+
     return (
-      <h1>/about page</h1>
+    <Grid 
+        container 
+        direction="row"
+        justify="center"
+        alignItems="flex-start"
+    >
+        <Grid item xs={12}>
+            <Paper className={classes.paper}>
+                All information about me you can get in LinkedIn or from my CV pdf file.
+            </Paper>
+        </Grid>
+        <Grid item xs={12}>
+            <Grid
+                container
+                direction="row"
+                justify="space-between"
+                className={classes.linkContainer}
+            >   
+                {data.links.map((link, i) => (<Grid key={i} xs={12} sm={6} md={3} className={classes.link}>
+                    <Button 
+                        variant="contained" 
+                        color="primary" 
+                        href={link.href} 
+                        target="_blank" 
+                        fullWidth
+                    >
+                        {link.title}
+                    </Button>
+                </Grid>))}
+            </Grid>
+        </Grid>
+    </Grid>
     );
   }
